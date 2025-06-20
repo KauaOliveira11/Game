a93468a33fb60bd726307f19c311537f7e44a73f
giovanikw <193257682+giovanikw@users.noreply.github.com>
2025-06-19 21:27:59 -0300

    Alteração nome de X e Y para ficar menos confuso, velocidade do personagem normalizada

--- game.lua
+++ game.lua
@@ -1,9 +1,11 @@
 -- game.lua
 
 -- Variáveis globais do game
-local x, y
+
+--player
+local playerX, playerY
 local tamanhoNormal = 200
-local tamanhoAbaixado = 20
+local tamanhoAbaixado = 100
 local tamanhoAtual = tamanhoNormal
 
 local velocidade = 100
@@ -12,11 +14,16 @@ local velocidade_corrida = 200
 local abaixando = false
 local estavaAbaixando = false
 
+--tamanho tela
+
+arenaLargura = 800
+arenaAltura = 600
+
 local imagem
 
 function game_load()
-    x = 100
-    y = 100
+    playerX = 100
+    playerY = 100
     imagem = love.graphics.newImage("Imagens/Personagem.png")
 end
 
@@ -24,10 +31,10 @@ function game_update(dt)
     abaixando = love.keyboard.isDown("lctrl") or love.keyboard.isDown("rctrl")
 
     if abaixando and not estavaAbaixando then
-        y = y + (tamanhoNormal - tamanhoAbaixado)
+        playerY = playerY + (tamanhoNormal - tamanhoAbaixado)
         tamanhoAtual = tamanhoAbaixado
     elseif not abaixando and estavaAbaixando then
-        y = y - (tamanhoNormal - tamanhoAbaixado)
+        playerY = playerY - (tamanhoNormal - tamanhoAbaixado)
         tamanhoAtual = tamanhoNormal
     end
 
@@ -36,22 +43,22 @@ function game_update(dt)
     local velocidadeAtual = love.keyboard.isDown("lshift") and velocidade_corrida or velocidade
 
     if love.keyboard.isDown("d") then 
-        x = x + velocidadeAtual * dt 
-    end
+        playerX = playerX + velocidadeAtual * dt % velocidade -- % normaliza pro máximo ser a
+    end                                           -- var velocidade(por ser hipotenusa)
     if love.keyboard.isDown("a") then 
-        x = x - velocidadeAtual * dt     
+        playerX = playerX - velocidadeAtual * dt % velocidade
     end
     if love.keyboard.isDown("w") then 
-        y = y - velocidadeAtual * dt 
+        playerY = playerY - velocidadeAtual * dt % velocidade
     end
     if love.keyboard.isDown("s") then 
-        y = y + velocidadeAtual * dt 
+        playerY = playerY + velocidadeAtual * dt % velocidade
     end
 end
 
 function game_draw()
     love.graphics.setColor(1, 1, 1) 
-    love.graphics.draw(imagem, x, y, 0, tamanhoAtual / imagem:getWidth(), tamanhoAtual / imagem:getHeight())
+    love.graphics.draw(imagem, playerX, playerY, 0, tamanhoAtual / imagem:getWidth(), tamanhoAtual / imagem:getHeight())
 end
 
 function game_keypressed(key)
